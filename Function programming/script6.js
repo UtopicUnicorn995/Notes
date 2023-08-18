ac318356ebdf1efee0a23fab8cdd3aadb94694b9
utopicUnicorn <christianbarrios995@gmail.com>
2023-08-18 18:16:01 +0800

    Notes-JS: compose and pipe notes

--- Function programming/script6.js
+++ Function programming/script6.js
@@ -1,8 +1,16 @@
 // Compose
 // Is the idea that any sort of data transformation that we do should be obvious
+// going from right to left
 
 // data --> fn --> data --> fn -->
 
-const compose = (f, g) => (data)
-
+const compose = (f, g) => (data) => f(g(data))
+const pipe = (f, g) => (data) => g(f(data))
+const multiplyBy3 = (num) => num * 3
+const makePositive = (num) => Math.abs(num)
 const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive)
+
+multiplyBy3AndAbsolute(-50)
+
+// Pipe is same as compose
+// going left to right
