3b33962956731ae2700d148b998e830beffc6c84
utopicUnicorn <christianbarrios995@gmail.com>
2023-08-30 17:20:57 +0800

    JS-notes: FS VS OOP notes final commit

--- FS VS OOP/script.js
+++ FS VS OOP/script.js
@@ -1,2 +1,28 @@
 // Inheritance is a super class that extended to smaller pieces that add or overwrite functionalities
 // Composition is smaller peices to create something bigger
+// Tight coupling problemt -> Fragile base class problem
+// Heirarchy problem
+// The subclass will have no choice but to inherit the characteristics of the main class
+
+
+//Programming paradigms
+// Same goals
+
+// ->OOP
+// Organizing the code into units would be called object oriented programming
+// An object is a box containing information and operations that are supposed to refer the same concept
+// pilars -> abstraction -> encapsulation -> inheritance -> polymorpism
+// common C-sharp, Python, Java
+// Few operations on common data
+//stateful
+
+
+
+// -> FP
+// Avoiding side effects and writing pure functions is called Functional programming
+// Code is essentially a combination of functions and data is immuateble which leads to writing programs with no side effects
+// function cannot change the outside world ad the output value of a function simply depends on the given arguments
+// Idea pure functions and composition
+// common in closure in Haskell
+// Many operations on fixed data
+// stateless
