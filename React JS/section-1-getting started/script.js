5d6289a369123e73d0836450a45c876c20307e97
utopicUnicorn <christianbarrios995@gmail.com>
2023-09-10 14:27:00 +0800

    react-notes: section 1 iniial notes

---
+++ React JS/section-1-getting started/script.js
@@ -0,0 +1,8 @@
+// Using just Javascript isn't a (Great) option
+// It quickly becomes cumbersome
+// It quickly becomes error-prone
+// It quickly becomes hard to maintain or edit
+// React offers a way simpler "mental model".
+
+// React == DECLARATIVE UI Programming
+// With react, you define the target UI state(s) - not the steps to get there Instead, React will figure out and perform the necessary steps
