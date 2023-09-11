9b8d5822dd4eabf86f446875de768ab79efb07c9
utopicUnicorn <christianbarrios995@gmail.com>
2023-09-11 18:35:43 +0800

    react-notes: added notes for import/export

---
+++ React JS/section-2-js-refresher/script.js
@@ -0,0 +1,15 @@
+// exporting variable using vanilla js
+// import {apiKey} from 'path of the file'
+// -> only works if there is a type module in the html
+
+// export default means that this value is the default export of the file
+// export default 'uasndasndasd'
+// only one thing can be exported per file
+
+// in file1.js import whateverNameYouwant from 'sourcepath/file2.js'
+// in file2.js export default 'chuchuchuchu'
+
+// can import more than 2 items in JS but only one default import
+
+// import * as utils from 'sourcepath/file.js'
+// util.whateverYouWantaccess
