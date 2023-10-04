ecb9438366620192dd806272d53ad687a8021de0
utopicUnicorn <christianbarrios995@gmail.com>
2023-10-04 19:45:08 +0800

    react-js notes: finished practice react

--- React-JS/section-4-practice/src/App.js
+++ React-JS/section-4-practice/src/App.js
@@ -2,6 +2,8 @@ import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
+import Concepts from './components/Concept';
+import Header from './components/Header'
 
 const concepts = [
   {
@@ -27,17 +29,15 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header item={keyConceptsImage}/>
       <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
-        </li>
+      {
+        concepts.map(concept => {
+          return (
+            <Concepts items={concept} key={concept.title}/>
+          )
+        })
+      }
       </ul>
     </div>
   );
---
+++ React-JS/section-4-practice/src/components/Concept.js
@@ -0,0 +1,15 @@
+
+const Concepts = (props) => {
+    console.log(props)
+    return(
+        <>
+        <li className="concept">
+          <img src={props.items.image} alt={props.items.title} />
+          <h2>{props.items.title}</h2>
+          <p>{props.items.description}</p>
+        </li>
+        </>
+    )
+}
+
+export default Concepts
