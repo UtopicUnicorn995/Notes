c7abd6cc2ebe31da27b359e6b8a55de7f4a46252
Ian Barrios <c.barrios@creativehope.co.jp>
2023-10-20 15:19:37 +0800

    js-notes: added async scripts

--- Vanilla JS/Asyncronous programming/script3.js
+++ Vanilla JS/Asyncronous programming/script3.js
@@ -1,3 +1,17 @@
 
 // async makes code more readable
 // async await is also a promise underneath the hood
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums',
+]
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(response => response.json())
+})).then(results => {
+    console.log(results[0])
+    console.log(results[1])
+    console.log(results[2])
+}).catch(() => console.log('error'))
