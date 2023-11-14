235dd5762141d29f4dc0f0d3bcee810591f34592
utopicUnicorn <christianbarrios995@gmail.com>
2023-11-14 17:33:46 +0800

    react-js-notes: separated codes into componentes

--- React-JS/section-1-getting-started/01-starting-project/src/App.jsx
+++ React-JS/section-1-getting-started/01-starting-project/src/App.jsx
@@ -1,10 +1,37 @@
 import Header from "./componentes/Header.jsx";
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./componentes/CoreConcept.jsx";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max + 1))
+}
+
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header genRandomInt={reactDescriptions[genRandomInt(2)]} />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+          <ul>
+            {/* {CORE_CONCEPTS.map(core_concept => (
+             <CoreConcept title={core_concept.title} description={core_concept.description} image={core_concept.image}/> 
+            ))} */}
+            <CoreConcept {...CORE_CONCEPTS[0]}/> 
+            <CoreConcept {...CORE_CONCEPTS[1]}/> 
+            <CoreConcept {...CORE_CONCEPTS[2]}/> 
+            <CoreConcept {...CORE_CONCEPTS[3]}/> 
+          </ul>
+        </section>
+        <section>
+          <h2>Examples</h2>
+          <menu>
+            
+          </menu>
+        </section>
       </main>
     </div>
   );
--- React-JS/section-1-getting-started/01-starting-project/src/componentes/Header.jsx
+++ React-JS/section-1-getting-started/01-starting-project/src/componentes/Header.jsx
@@ -1,10 +1,14 @@
-export default function Header(){
+import reactImg from '../assets/react-core-concepts.png'
+import './Header.css'
+export default function Header({genRandomInt}){
     return (
         <header>
-          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+          {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+          {/* The code above is not a proper usage, might get lost once deployed*/}
+          <img src={reactImg} alt="Stylized atom" />
           <h1>React Essentials</h1>
           <p>
-            Fundamental React concepts you will need for almost any app you are
+            {genRandomInt} React concepts you will need for almost any app you are
             going to build!
           </p>
         </header>
