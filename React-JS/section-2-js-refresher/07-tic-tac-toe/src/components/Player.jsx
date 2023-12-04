5c18ac85f396b488d43a32dca05d142b9b3cfce7
utopicUnicorn <christianbarrios995@gmail.com>
2023-12-04 18:34:43 +0800

    react-js: Created the gameboard

--- React-JS/section-2-js-refresher/07-tic-tac-toe/src/components/Player.jsx
+++ React-JS/section-2-js-refresher/07-tic-tac-toe/src/components/Player.jsx
@@ -1,28 +1,34 @@
 import { useState } from "react"
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
     const handleEdit = () => {
         setIsEditing(editing => !editing) // better practice
         // Inverts the value
         // When updatting the state based on the previous state, pass a function to your state
+        // Two way binding
     }
 
-    let playerName = <span className="player-name">{name}</span>
+    const handleChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
     let btnCaption = 'Edit'
 
     if(isEditing){
-        playerName = <input type="text" required value={name}/>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
         btnCaption = "Save"
     }
 
     return(
         <li>
             <span className="player">
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEdit}>{btnCaption}</button>
-          </li>
+        </li>
     )
 }
