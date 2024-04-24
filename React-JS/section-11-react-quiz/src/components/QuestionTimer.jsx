1cfe9c9ea49850f7061a62c50bb2b5b8e3280691
utopicUnicorn <christianbarrios995@gmail.com>
2024-04-24 16:00:42 +0800

    React notes: quiz game

--- React-JS/section-11-react-quiz/src/components/QuestionTimer.jsx
+++ React-JS/section-11-react-quiz/src/components/QuestionTimer.jsx
@@ -1,19 +1,30 @@
 import { useState, useEffect } from "react"
 
-export default function QuestionTimer({timeout, onTimeout}){
+export default function QuestionTimer({timeout, onTimeout, mode}){
     const [remainingTime, setRemainingTime] = useState(timeout)
 
     useEffect(() => {
-        setTimeout(onTimeout, timeout)
+        console.log('setting timeout')
+        const timer = setTimeout(onTimeout, timeout)
+        return () => {
+            clearTimeout(timer)
+        }
     }, [timeout, onTimeout])
 
+
+
     useEffect(() => {
-        setInterval(() => {
+        console.log('setting interval')
+        const interval = setInterval(() => {
             setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
         },100)
+
+        return () => {
+            clearInterval(interval)
+        }
     }, [])
 
     return (
-        <progress id="question-timer" max={timeout} value={remainingTime}/>
+        <progress id="question-timer" max={timeout} value={remainingTime} className={mode}/>
     )
 }
--- React-JS/section-11-react-quiz/src/components/Quiz.jsx
+++ React-JS/section-11-react-quiz/src/components/Quiz.jsx
@@ -1,51 +1,46 @@
-import { useState } from "react"
+import { useState, useCallback } from "react";
 
-import QuizCompleteImg from '../assets/quiz-complete.png'
-import QuestionTimer from "./QuestionTimer.jsx"
-import QUESTIONS from '../questions.js'
+import QUESTIONS from "../questions.js";
+import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  // Not optimal for Max
+  // Best practice -> manage less states as possible; better to derive states as much as possible
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setUserAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+    },
+    []
+  );
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers}/>
+  }
 
 
-export default function Quiz(){
-    const [userAnswers, setUserAnswers] = useState([])
-    // Not optimal for Max
-    // Best practice -> manage less states as possible; better to derive states as much as possible
 
-    const activeQuestionIndex = userAnswers.length
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
-
-    function handleSelectAnswer(selectedAnswer){
-        setUserAnswers((prevUserAnswers) => {
-            return [...prevUserAnswers, selectedAnswer]
-        })
-    }
-
-    if(quizIsComplete){
-        return <div id="summary">
-            <img src={QuizCompleteImg} alt="Trophy Icon" />
-            <h2>Quiz Completed!</h2>
-        </div>
-    }
-
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(() => Math.random() - 0.5)
-
-    return(
-       <div id="quiz">
-            <div id="question">
-            <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
-            <h2>
-                {QUESTIONS[activeQuestionIndex].text}
-            </h2>
-            <ul id="answers">
-                {shuffledAnswers.map((answer) => (
-                    <li key={{answer}} className="answer">
-                        <button onClick={() => handleSelectAnswer(answer)}>
-                            {answer}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            </div>
-       </div>
-    )
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        handleSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
 }
