2ac68c3deade7b571b2babd09f6e34fd626afa51
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-06 15:00:53 +0800

    installed millionjs

--- React-JS/section-12-optimization/src/components/Counter/CounterHistory.jsx
+++ React-JS/section-12-optimization/src/components/Counter/CounterHistory.jsx
@@ -6,6 +6,7 @@ function HistoryItem({ count }) {
   log('<HistoryItem /> rendered', 3);
 
   const [selected, setSelected] = useState(false);
+  
 
   function handleClick() {
     setSelected((prevSelected) => !prevSelected);
@@ -20,12 +21,15 @@ function HistoryItem({ count }) {
 
 export default function CounterHistory({ history }) {
   log('<CounterHistory /> rendered', 2);
-
+  console.log(history)
   return (
     <ol>
-      {history.map((count, index) => (
-        <HistoryItem key={index} count={count} />
-      ))}
+      {history.map((count) =>
+        (
+          <HistoryItem key={count.id} count={count.value} />
+          //Keys are important in a list
+          //
+        ))}
     </ol>
   );
 }
