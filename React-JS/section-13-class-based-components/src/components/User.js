37d17d31bb2f4d1459d32613eb49666ddfd0cb49
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-10 14:20:39 +0800

    react-js: added class based component notes

---
+++ React-JS/section-13-class-based-components/src/components/User.js
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+import classes from "./User.module.css";
+
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  } // Render is a specific method by react which react will call when it finds a component being used in JSXcode
+  //to find what will be showned in the screen
+}
+
+// const User = (props) => {
+// return <li className={classes.user}>{this.props.name}</li>;
+// };
+
+export default User;
