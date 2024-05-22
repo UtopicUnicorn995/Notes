858d65b0ac2ed583220f55c28dbd9333b408febb
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-22 23:28:35 +0800

    react-js: returned using state logging in

--- React-JS/section-16-forms/src/components/StateLogin.jsx
+++ React-JS/section-16-forms/src/components/StateLogin.jsx
@@ -5,21 +5,26 @@ export default function Login() {
   // const [enteredPassword, setEnteredPassword] = useState("");
   const [enteredValues, setEnteredValues] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  console.log(enteredValues)
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues => ({
-      ...prevValues, [identifier]: value
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
     }));
   }
 
+  function handleEmailBlur(identifier){
+    
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -32,9 +37,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            onChange={(e) => handleInputChange('email', e.target.value)}
-            value={enteredValues['email']}
+            onBlur={handleEmailBlur} //Activates whenever the input loses focus
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredValues["email"]}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -43,8 +52,8 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            onChange={(e) => handleInputChange('password', e.target.value)}
-            value={enteredValues['password']}
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredValues["password"]}
           />
         </div>
       </div>
