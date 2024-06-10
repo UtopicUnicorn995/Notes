0a918f92087f70cf29410894485b22e539c34193
utopicUnicorn <christianbarrios995@gmail.com>
2024-06-10 23:46:20 +0800

    react-js: added userprogres context store

---
+++ React-JS/section-17-project-2/src/components/Cart.jsx
@@ -0,0 +1,32 @@
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Modal from "./Modal";
+import Button from "../UI/Button";
+import { useContext } from "react";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart" >
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to checkout</Button>
+      </p>
+    </Modal>
+  );
+}
---
+++ React-JS/section-17-project-2/src/store/UserProgressContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+    progress: '', // 'cart', 'checkout'
+    showCart: () => {},
+    hideCart: () => {},
+    showCheckout: () => {},
+    hideCheckout: () => {}
+})
+
+export function UserProgressContextProvider({children}){
+    const [userProgress, setUserProgress] = useState()
+}
